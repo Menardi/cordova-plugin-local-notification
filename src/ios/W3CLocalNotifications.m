e688392516ff23a3421771caad4b73c580819e5e
Niek <niek@emesa.nl>
2017-09-20 16:43:23 +0200

    simplify loading of attachment enable loading of custom mp3 sound

--- src/ios/W3CLocalNotifications.m
+++ src/ios/W3CLocalNotifications.m
@@ -8,63 +8,54 @@
 
 @synthesize callbackId;
 
-- (NSString *)fileExtensionForMediaType:(NSString *)type {
-    NSString *ext = type;
-
-    if ([type isEqualToString:@"image"]) {
-        ext = @"jpg";
-    }
-
-    if ([type isEqualToString:@"video"]) {
-        ext = @"mp4";
-    }
-
-    if ([type isEqualToString:@"audio"]) {
-        ext = @"mp3";
-    }
-
-    return [@"." stringByAppendingString:ext];
-}
-
-- (void)loadAttachmentForUrlString:(NSString *)urlString withType:(NSString *)type
-                 completionHandler:(void(^)(UNNotificationAttachment *))completionHandler  {
-
-    __block UNNotificationAttachment *attachment = nil;
+- (UNNotificationAttachment*)loadAttachment:(NSString *)urlString {
     NSURL *attachmentURL = [NSURL URLWithString:urlString];
-    NSString *fileExt = [self fileExtensionForMediaType:type];
-
-    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-    [[session downloadTaskWithURL:attachmentURL
-        completionHandler:^(NSURL *temporaryFileLocation, NSURLResponse *response, NSError *error) {
-            if (error != nil) {
-                NSLog(@"%@", error.localizedDescription);
+    NSData *data = [NSData dataWithContentsOfURL:attachmentURL];
+    if (data) {
+        NSURL *localURL = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:[attachmentURL lastPathComponent]]];
+        NSLog(@"writing notification attachment to tmp: %@",localURL);
+        if([data writeToURL:localURL atomically:NO]) {
+            NSError *attachmentError = nil;
+            UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"" URL:localURL options:nil error:&attachmentError];
+            if (attachmentError) {
+                NSLog(@"error adding attachment: %@", attachmentError.localizedDescription);
             } else {
-                NSFileManager *fileManager = [NSFileManager defaultManager];
-                NSURL *localURL = [NSURL fileURLWithPath:[temporaryFileLocation.path stringByAppendingString:fileExt]];
-                [fileManager moveItemAtURL:temporaryFileLocation toURL:localURL error:&error];
-
-                NSError *attachmentError = nil;
-                attachment = [UNNotificationAttachment attachmentWithIdentifier:@"" URL:localURL options:nil error:&attachmentError];
-                if (attachmentError) {
-                    NSLog(@"%@", attachmentError.localizedDescription);
-                }
+                return attachment;
             }
-            completionHandler(attachment);
-        }] resume];
+        }
+    }
+    return NULL;
 }
 
-- (void)show:(CDVInvokedUrlCommand*)command {
+- (UNNotificationSound*)loadSound:(NSString *)urlString {
+    NSURL *attachmentURL = [NSURL URLWithString:urlString];
+    NSData *data = [NSData dataWithContentsOfURL:attachmentURL];
+    if (data) {
+        NSString *soundsDir = [NSHomeDirectory() stringByAppendingString:@"/Library/Sounds/"];
+        NSFileManager *fileManager= [NSFileManager defaultManager];
+        if(![fileManager fileExistsAtPath:soundsDir isDirectory:NULL]) {
+            if(![fileManager createDirectoryAtPath:soundsDir withIntermediateDirectories:YES attributes:nil error:NULL]) {
+                NSLog(@"Cannot create sounds folder: %@", soundsDir);
+                return [UNNotificationSound defaultSound];
+            }
+        }
+        NSURL *localURL = [NSURL fileURLWithPath:[soundsDir stringByAppendingPathComponent:[attachmentURL lastPathComponent]]];
+        NSLog(@"Sound local url: %@", localURL);
+        if([data writeToURL:localURL atomically:NO]) {
+            return [UNNotificationSound soundNamed:[attachmentURL lastPathComponent]];
+        }
+    }
+    return [UNNotificationSound defaultSound];;
+}
+
+- (void)realshow:(CDVInvokedUrlCommand*)command {
     self.callbackId = command.callbackId;
-
     UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-
     UNMutableNotificationContent *content = [UNMutableNotificationContent new];
     content.title = [command.arguments objectAtIndex:0];
     content.body = [command.arguments objectAtIndex:3];
-    content.sound = [UNNotificationSound defaultSound];
     content.badge = [NSNumber numberWithInt:1];
     content.userInfo = @{@"deep_link_action": [command.arguments objectAtIndex:8]};
-
     NSString *identifier = [command.arguments objectAtIndex:4];
     double ms = [[command.arguments objectAtIndex:7] doubleValue];
     NSDate *when = [NSDate dateWithTimeIntervalSince1970:ms / 1000.0];
@@ -72,42 +63,40 @@
     //NSLog(@"Notification set at: %@",[dateFormatter stringFromDate:when]);
     NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
     NSDateComponents *date = [calendar components:(NSCalendarUnitYear  |
-                                                     NSCalendarUnitMonth |
-                                                     NSCalendarUnitDay   |
-                                                     NSCalendarUnitHour  |
-                                                     NSCalendarUnitMinute |
-                                                     NSCalendarUnitSecond) fromDate:when];
+                                                   NSCalendarUnitMonth |
+                                                   NSCalendarUnitDay   |
+                                                   NSCalendarUnitHour  |
+                                                   NSCalendarUnitMinute |
+                                                   NSCalendarUnitSecond) fromDate:when];
     UNCalendarNotificationTrigger* trigger = [UNCalendarNotificationTrigger triggerWithDateMatchingComponents:date repeats:NO];
-
     [content setValue:@YES forKey:@"shouldAlwaysAlertWhileAppIsForeground"];
-
     NSString *imageUrl = [command.arguments objectAtIndex:5];
-    NSLog(@"loading icon image: %@",imageUrl);
-
-    // load the attachment
-    [self loadAttachmentForUrlString:imageUrl
-        withType:@"jpg"
-        completionHandler:^(UNNotificationAttachment *attachment) {
-            if (attachment) {
-                NSLog(@"loading icon image done adding to notification: %@",imageUrl);
-                content.attachments = [NSArray arrayWithObject:attachment];
-            }
-
-            UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger ];
-            [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
-                if (error != nil) {
-                    NSLog(@"Something went wrong: %@",error);
-                    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                } else {
-                     NSLog(@"alarm scheduled");
-                     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"show"];
-                    [pluginResult setKeepCallbackAsBool:YES];
-                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-                }
-            }];
+    UNNotificationAttachment *imageAttachment = [self loadAttachment:imageUrl];
+    if (imageAttachment) {
+        NSLog(@"loading notification image: %@",imageUrl);
+        content.attachments = @[imageAttachment];
+    }
+    NSString *soundUrl = [command.arguments objectAtIndex:6];
+    NSLog(@"loading notification sound: %@",soundUrl);
+    content.sound = [self loadSound:soundUrl];
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger ];
+    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+        if (error != nil) {
+            NSLog(@"Something went wrong: %@",error);
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        } else {
+            NSLog(@"alarm scheduled");
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"show"];
+            [pluginResult setKeepCallbackAsBool:YES];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         }
-    ];
+    }];
+}
+
+
+- (void)show:(CDVInvokedUrlCommand*)command {
+    [self performSelectorInBackground:@selector(realshow:) withObject:command];
 }
 
 - (void)close:(CDVInvokedUrlCommand*)command {
