ce6089a00bdbd0da67f26b3e5f04e627fe747ff8
Gearóid <hello@gearoid.me>
2018-08-31 16:29:42 +0900

    Notification sound on iOS

--- src/ios/W3CLocalNotifications.m
+++ src/ios/W3CLocalNotifications.m
@@ -8,61 +8,35 @@
 
 @synthesize callbackId;
 
-- (UNNotificationAttachment*)loadAttachment:(NSString *)urlString {
-    NSURL *attachmentURL = [NSURL URLWithString:urlString];
-    NSData *data = [NSData dataWithContentsOfURL:attachmentURL];
-    if (data) {
-        NSURL *localURL = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:[attachmentURL lastPathComponent]]];
-        NSLog(@"writing notification attachment to tmp: %@",localURL);
-        if([data writeToURL:localURL atomically:NO]) {
-            NSError *attachmentError = nil;
-            UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"" URL:localURL options:nil error:&attachmentError];
-            if (attachmentError) {
-                NSLog(@"error adding attachment: %@", attachmentError.localizedDescription);
-            } else {
-                return attachment;
-            }
-        }
+- (UNNotificationSound*)getSound:(NSString*)path {
+    if([path hasPrefix:@"file://"]) {
+        NSString* localPath = [path stringByReplacingOccurrencesOfString:@"file://" withString:@"/www/"];
+        
+        return [UNNotificationSound soundNamed:localPath];
     }
-    return NULL;
-}
-
-- (UNNotificationSound*)loadSound:(NSString *)urlString {
-    NSURL *attachmentURL = [NSURL URLWithString:urlString];
-    NSData *data = [NSData dataWithContentsOfURL:attachmentURL];
-    if (data) {
-        NSString *soundsDir = [NSHomeDirectory() stringByAppendingString:@"/Library/Sounds/"];
-        NSFileManager *fileManager= [NSFileManager defaultManager];
-        if(![fileManager fileExistsAtPath:soundsDir isDirectory:NULL]) {
-            if(![fileManager createDirectoryAtPath:soundsDir withIntermediateDirectories:YES attributes:nil error:NULL]) {
-                NSLog(@"Cannot create sounds folder: %@", soundsDir);
-                return [UNNotificationSound defaultSound];
-            }
-        }
-        NSURL *localURL = [NSURL fileURLWithPath:[soundsDir stringByAppendingPathComponent:[attachmentURL lastPathComponent]]];
-        NSLog(@"Sound local url: %@", localURL);
-        if([data writeToURL:localURL atomically:NO]) {
-            return [UNNotificationSound soundNamed:[attachmentURL lastPathComponent]];
-        }
-    }
-    return [UNNotificationSound defaultSound];;
+    
+    return [UNNotificationSound defaultSound];
 }
 
 - (void)realshow:(CDVInvokedUrlCommand*)command {
     self.callbackId = command.callbackId;
+    
     UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
     UNMutableNotificationContent *content = [UNMutableNotificationContent new];
+    
     content.title = [command.arguments objectAtIndex:1];
     content.body = [command.arguments objectAtIndex:2];
     content.badge = [NSNumber numberWithInt:1];
+    
     NSString *identifier = [command.arguments objectAtIndex:0];
     NSString *imageUrl = [command.arguments objectAtIndex:4];
     NSString *soundUrl = [command.arguments objectAtIndex:5];
+    
     content.userInfo = @{@"deep_link_action": [command.arguments objectAtIndex:6], @"imageUrl": imageUrl, @"soundUrl": soundUrl};
+    
     double ms = [[command.arguments objectAtIndex:3] doubleValue];
     NSDate *when = [NSDate dateWithTimeIntervalSince1970:ms / 1000.0];
-    NSISO8601DateFormatter *dateFormatter = [[NSISO8601DateFormatter alloc] init];
-    NSLog(@"Notification set at: %@",[dateFormatter stringFromDate:when]);
+    
     NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
     NSDateComponents *date = [calendar components:(NSCalendarUnitYear  |
                                                    NSCalendarUnitMonth |
@@ -70,30 +44,29 @@
                                                    NSCalendarUnitHour  |
                                                    NSCalendarUnitMinute |
                                                    NSCalendarUnitSecond) fromDate:when];
+    
     UNCalendarNotificationTrigger* trigger = [UNCalendarNotificationTrigger triggerWithDateMatchingComponents:date repeats:NO];
+    
     //[content setValue:@YES forKey:@"shouldAlwaysAlertWhileAppIsForeground"];
-//    UNNotificationAttachment *imageAttachment = [self loadAttachment:imageUrl];
-//    if (imageAttachment) {
-//        NSLog(@"loading notification image: %@",imageUrl);
-//        content.attachments = @[imageAttachment];
-//    }
-//    NSLog(@"loading notification sound: %@",soundUrl);
-//    content.sound = [self loadSound:soundUrl];
+    
+    NSLog(@"Loading notification sound: %@",soundUrl);
+    content.sound = [self getSound:soundUrl];
+    
     UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger ];
+    
     [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
         if (error != nil) {
             NSLog(@"Something went wrong: %@",error);
             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         } else {
-            NSLog(@"Notification scheduled: %@", trigger.dateComponents);
+            NSLog(@"Notification scheduled (tag: %@)", identifier);
             CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"show"];
             [pluginResult setKeepCallbackAsBool:YES];
             [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
         }
     }];
 }
-
 
 - (void)show:(CDVInvokedUrlCommand*)command {
     [self performSelectorInBackground:@selector(realshow:) withObject:command];
